cbc8fb4b7a9b38ca303c303764efbdce6390a619
Karen Lundgren <karen.e.lundgren@gmail.com>
2015-04-07 17:13:17 -0600

    Fixed spec/dummy/config/routes file.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
   mount PaidUp::Engine => '/', :as => 'PaidUp'
-  mount PaidUp::Engine, at: '/'
   devise_for :users
-  root to: "home#index"
   
 end
