a556bafc7dd19e98ac8dd1b5da39df1536cc2290
Karen Lundgren <karen.e.lundgren@gmail.com>
2015-04-16 17:10:09 -0600

    Added bootswitch dependency to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda/matchers'
 require 'factory_girl_rails'
+require 'bootswitch'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
