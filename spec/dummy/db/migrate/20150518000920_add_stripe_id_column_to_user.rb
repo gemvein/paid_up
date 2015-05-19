576ef26bf846a371198ace0748000069d40a088a
Karen Lundgren <karen.e.lundgren@gmail.com>
2015-05-19 10:48:25 -0600

    Corrected errors in stripe_id migration

--- spec/dummy/db/migrate/20150518000920_add_stripe_id_column_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddStripeIdColumnToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :stripe_id, :string
-    add_index :users, :stripe_id
-  end
-end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518000920) do
+ActiveRecord::Schema.define(version: 20150519164355) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "user_id"
@@ -70,6 +70,5 @@ ActiveRecord::Schema.define(version: 20150518000920) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["stripe_id"], name: "index_users_on_stripe_id"
 
 end
