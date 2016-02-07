acc3c93e2536497e28baacd1ec59a6787d16f2ab
Karen Lundgren <karen.e.lundgren@gmail.com>
2016-02-07 11:52:04 -0700

    Version bump to 0.8.0

--- db/migrate/20160207113800_create_paid_up_plan_feature_settings_table.rb
+++ db/migrate/20160207113800_create_paid_up_plan_feature_settings_table.rb
@@ -1,7 +1,7 @@
 class CreatePaidUpPlanFeatureSettingsTable < ActiveRecord::Migration
   def change
     create_table :paid_up_plan_feature_settings do |t|
-      t.references :plan, index: true, foreign_key: true
+      t.references :paid_up_plan, index: true, foreign_key: true
       t.string :feature, index: true, foreign_key: true
       t.integer :setting
     end
--- spec/dummy/db/migrate/20150523010840_add_stripe_id_column_to_users.paid_up.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from paid_up (originally 20150519164237)
-class AddStripeIdColumnToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :stripe_id, :string, index: true
-  end
-end
