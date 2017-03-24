d8305d03a48fac238f9f981bb0f2c6729964067d
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-03-24 13:16:10 -0600

    Fixed Metrics/BlockLength for rubocop

--- app/models/paid_up/ability.rb
+++ app/models/paid_up/ability.rb
@@ -4,53 +4,51 @@ module PaidUp
     include CanCan::Ability
 
     def initialize_paid_up(user)
-      features = PaidUp::Feature.all
-      features.each do |feature|
-        case feature.setting_type
-        when 'table_rows'
-          can :index, feature.feature_model
-          can :show, feature.feature_model, &:enabled
-          if user.table_rows_allowed(feature.slug) > 0 ||
-             user.table_rows_unlimited?(feature.slug)
-            can :manage, feature.feature_model, user: user
-            can :own, feature.feature_model
-            cannot :create, feature.feature_model
-            user.table_rows_remaining(feature.slug) > 0 &&
-              can([:create, :new], feature.feature_model)
-          else
-            cannot :delete, feature.feature_model
-            cannot :update, feature.feature_model
-            cannot :own, feature.feature_model
-            cannot :create, feature.feature_model
-          end
-        when 'rolify_rows'
-          can :index, feature.feature_model
-          can :show, feature.feature_model, &:enabled
-          if user.rolify_rows_allowed(feature.slug) > 0 ||
-             user.rolify_rows_unlimited?(feature.slug)
-            can(
-              :manage,
-              feature.feature_model,
-              id: feature.feature_model.with_role(:owner, user).ids
-            )
-            can :own, feature.feature_model
-            cannot :create, feature.feature_model
-            user.rolify_rows_remaining(feature.slug) > 0 &&
-              can([:create, :new], feature.feature_model)
-          else
-            cannot :delete, feature.feature_model
-            cannot :update, feature.feature_model
-            cannot :own, feature.feature_model
-            cannot :create, feature.feature_model
-          end
-        when 'boolean'
-          if user.plan.feature_setting feature.slug
-            can :use, feature.slug.to_sym
-          end
-        else
-          raise(:unknown_feature_type.l)
-        end
+      PaidUp::Feature.all.each do |feature|
+        method = "enable_#{feature.setting_type}".to_sym
+        send(method, user, feature)
       end
+    end
+
+    private
+
+    def enable_table_rows(user, feature)
+      slug = feature.slug
+      model = feature.feature_model
+      can :index, model
+      can :show, model, &:enabled
+      if user.table_rows_allowed(slug) > 0 ||
+         user.table_rows_unlimited?(slug)
+        can :manage, model, user: user
+        can :own, model
+        cannot :create, model
+        user.table_rows_remaining(slug) > 0 &&
+          can([:create, :new], model)
+      else
+        cannot [:delete, :update, :own, :create], model
+      end
+    end
+
+    def enable_rolify_rows(user, feature)
+      slug = feature.slug
+      model = feature.feature_model
+      can :index, model
+      can :show, model, &:enabled
+      if user.rolify_rows_allowed(slug) > 0 ||
+         user.rolify_rows_unlimited?(slug)
+        can :manage, model, id: model.with_role(:owner, user).ids
+        can :own, model
+        cannot :create, model
+        user.rolify_rows_remaining(slug) > 0 &&
+          can([:create, :new], model)
+      else
+        cannot [:delete, :update, :own, :create], model
+      end
+    end
+
+    def enable_boolean(user, feature)
+      slug = feature.slug
+      can :use, slug.to_sym if user.plan.feature_setting slug
     end
   end
 end
--- spec/dummy/config/initializers/paid_up.rb
+++ spec/dummy/config/initializers/paid_up.rb
@@ -4,30 +4,22 @@ PaidUp.configure do |config|
   config.free_plan_stripe_id = 'free-plan'
 
   PaidUp.add_feature(
-    slug: 'ad_free',
-    title: 'Ad Free',
-    description: 'Are ads removed from the site with this plan?',
-    setting_type: 'boolean'
+    slug: 'ad_free', title: 'Ad Free', setting_type: 'boolean',
+    description: 'Are ads removed from the site with this plan?'
   )
   PaidUp.add_feature(
-    slug: 'groups',
-    title: 'Groups',
-    description: 'How many groups are allowed with this plan?',
+    slug: 'groups', title: 'Groups', setting_type: 'rolify_rows',
+    description: 'How many groups are allowed with this plan?'
     # Enables table row counting that is enabled by a positive value
     # for the PaidUp::PlanFeatureSetting.setting associated with this
     # PaidUp::Feature
-    setting_type: 'rolify_rows'
   )
   PaidUp.add_feature(
-    slug: 'doodads',
-    title: 'Doodads',
-    description: 'How many doodads included with this plan?',
-    setting_type: 'table_rows'
+    slug: 'doodads', title: 'Doodads', setting_type: 'table_rows',
+    description: 'How many doodads included with this plan?'
   )
   PaidUp.add_feature(
-    slug: 'posts',
-    title: 'Posts',
-    description: 'How many posts included with this plan?',
-    setting_type: 'table_rows'
+    slug: 'posts', title: 'Posts', setting_type: 'table_rows',
+    description: 'How many posts included with this plan?'
   )
 end
